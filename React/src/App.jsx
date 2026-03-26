a1545202c5d77e3a2102ca0bba10cbb52b1cb26d
Himani <himanisaini032005@gmail.com>
2026-03-26 12:45:35 +0530

    M

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </div>
         <div>
           <h1>Get started</h1>
+          <h2>simran</h2>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
